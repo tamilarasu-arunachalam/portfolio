0c0ec55b7a55fd666429d74b8260d872f3def39c
TA <tamilarasuva444@gmail.com>
2021-06-04 21:23:00 +0530

    fixed footer added

--- js/script.js
+++ js/script.js
@@ -1,25 +1,41 @@
 // preloader
-$(window).on('load', function() { // makes sure the whole site is loaded 
-  $('#status').fadeOut(); // will first fade out the loading animation 
-  $('#preloader').delay(1000).fadeOut('slow'); // will fade out the white DIV that covers the website. 
-  $('body').delay(1000).css({'overflow':'visible'});
-})
-//auto typing 
-var typed = new Typed('#typed',{
-  strings:["Tamilarasu","a Web Developer"],
+$(window).on("load", function () {
+  // makes sure the whole site is loaded
+  $("#status").fadeOut(); // will first fade out the loading animation
+  $("#preloader").delay(1000).fadeOut("slow"); // will fade out the white DIV that covers the website.
+  $("body").delay(1000).css({ overflow: "visible" });
+});
+
+//auto typing
+var typed = new Typed("#typed", {
+  strings: ["Tamilarasu", "a Web Developer"],
   backSpeed: 40,
   typeSpeed: 30,
   loop: true,
-  shuffle: false
+  shuffle: false,
 });
+
 //active nav item
-$(".nav-item").on("click", function(e){
+$(".nav-item").on("click", function (e) {
   // Remove class active form all li.nav-tiem
   $("li.nav-item").removeClass("active");
-  // Add Class to current Element. 
+  // Add Class to current Element.
   $(this).addClass("active");
 });
+
 // close left slide menu after clicking nav-item
-$('.navbar-nav>li>a').on('click', function(){
-  $('.navbar-collapse').collapse('hide');
+$(".navbar-nav>li>a").on("click", function () {
+  $(".navbar-collapse").collapse("hide");
+});
+
+//sticky footer
+$(window).scroll(function () {
+  var sticky = $(".sticky"),
+    scroll = $(document).height() - $(window).height() - $(window).scrollTop();
+
+  if (scroll >= 100) {
+    sticky.addClass("fixed");
+  } else {
+    sticky.removeClass("fixed");
+  }
 });
