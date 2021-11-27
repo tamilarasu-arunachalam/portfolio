0af0c12a51aa075cda82ae50a39f9fc22f181fe5
TA <tamilarasuva444@gmail.com>
2021-11-27 22:11:14 +0530

    updated commit

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 $(window).on("load", function () {
   // makes sure the whole site is loaded
   $("#status").fadeOut(); // will first fade out the loading animation
-  $("#preloader").delay(1000).fadeOut("slow"); // will fade out the white DIV that covers the website.
+  $("#preloader").delay(2000).fadeOut("slow"); // will fade out the white DIV that covers the website.
   $("body").delay(1000).css({ overflow: "visible" });
 });
 
